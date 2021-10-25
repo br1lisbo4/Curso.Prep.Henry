7aa650d3b6a6866c56dc02eea4cd00c07c8bb758
BRUNO O. LISBOA <bolisboa@secheep.local>
2021-10-25 12:23:46 -0300

    van 2s..

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -7,13 +7,13 @@ function crearGato(nombre, edad) {
   // Devuelve el objeto
   // Tu código:
   var objeto = {
-    nombre: "nombre",
-    edad: "edad",
+    nombre: nombre,
+    edad: edad,
     meow: function(){
       return "Meow!"
     }
   }
-  return objeto
+  return objeto;
 }
 
 
@@ -22,6 +22,8 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto.property = "null"
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
